941f6078f3c20885f248235efe85e6f57d2448c2
madhurn1 <madhurnutulapati@gmail.com>
2023-11-26 12:35:20 -0500

    export() method done?

--- src/main/java/rupizza/StoreOrders.java
+++ src/main/java/rupizza/StoreOrders.java
@@ -1,7 +1,11 @@
 package rupizza;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 public class StoreOrders {
     private static int nextAvailableOrderNum = 1;
     private List<Order> orders;
@@ -20,7 +24,18 @@ public class StoreOrders {
     public void cancelOrder(Order order) {
         orders.remove(order);
     }
-    public void export(String filePath){
-
+    public void export(String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Order order : orders) {
+                writer.write("Order number: " + order.getOrderNumber() + "\n");
+                for(Pizza pizza : order.getPizzas()){
+                    writer.write("Pizza type: " + pizza.getClass().getSimpleName()+"\n");
+                    //Anything else to add ?
+                }
+                writer.write("Total: $"+order.calcTotalPrice()+ "/n/n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error exporting saving the store orders to an external text file: " + e.getMessage());
+        }
     }
 }
